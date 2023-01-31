d0eeabc8861b823b82fb9568fb0dc0bcbe9a4b1b
Amar <rautamar705@gmail.com>
2023-01-31 11:42:02 +0530

    updated second largest no

--- HackerRank/Day_2/03_second_largest.py
+++ HackerRank/Day_2/03_second_largest.py
@@ -1,10 +1,13 @@
-arr = [2,3,10,15,13,5,6,6]
-larg = min(arr)
-second_l = min(arr)
-for i in arr:
-    if(i>larg and i>second_l):
-        second_l = larg
-        larg = i
-    elif(i<larg and i>second_l):
-        second_l = i
-print(second_l)
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    largest = min(arr)
+    second_largest = min(arr)
+    for i in arr:
+        if(i>largest and i>second_largest):
+            second_largest = largest
+            largest = i
+        elif(i<largest and i>second_largest):
+            second_largest = i
+    print(second_largest)
+    # arr = [2,3,10,15,13,5,6,6]
