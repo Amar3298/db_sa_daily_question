4cefeb96bf65486e9d854d98f9bd3491e51fa58f
Amar <rautamar705@gmail.com>
2023-02-03 10:05:06 +0530

    04 Split and join learned

---
+++ Things_learned/04_split_join.py
@@ -0,0 +1,10 @@
+# Split splits the line according to the need eg split the string when " " 
+str1 = "Dragon Ball is fav"
+list1 = str1.split(" ") # it will split the string and store it in the list
+print(list1)
+
+# join is use to join list with some character 
+join_a = "-"
+res = join_a.join(list1) # it will join the list using "" and covert it into string
+print(res)
+
