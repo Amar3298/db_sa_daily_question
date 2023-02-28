a3c9d7d40bb473476c0e6a3d9c4a5594b67d8d07
Amar <rautamar705@gmail.com>
2023-02-28 12:14:23 +0530

    Q1 28th Feb 2023 Leet Code

---
+++ Leat_code/02_single_number/db.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res = res ^ i
+        return res
+
+obj = Solution()
+res = obj.singleNumber([2,2,1])
+print(res)
