5717908fcd7dc22e50c5b655c559bcbb80e4eb39
Amar <rautamar705@gmail.com>
2023-03-31 22:31:58 +0530

    Q1 31st March 2023 Leet Code

---
+++ Leat_code/07_concatination_of_array/db.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getConcatenation(self, nums):
+        temp_ans = nums
+        res = nums + temp_ans
+        return res
+    
+obj = Solution()
+res = obj.getConcatenation([1,2,1])
+print(res)
