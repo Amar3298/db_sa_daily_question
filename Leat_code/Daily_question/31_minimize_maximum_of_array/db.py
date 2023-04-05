0641792fa2d4f9a2bdf64b71cf5d5e647be74743
Amar <rautamar705@gmail.com>
2023-04-05 13:32:51 +0530

    Q1 05th April 2023 Leet Code

---
+++ Leat_code/Daily_question/31_minimize_maximum_of_array/db.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def minimizeArrayValue(self, nums):
+        res = total = nums[0]
+        for i in range(1,len(nums)):
+            total += nums[i]
+            res = max(res,math.ceil(total/(i+1)))
+        return res  
