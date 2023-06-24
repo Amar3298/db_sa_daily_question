73584fa9480be88cff5f6da8cf28149a642bb952
Amar <rautamar705@gmail.com>
2023-06-24 11:02:24 +0530

    Q2 24th June 2023 GFG

---
+++ Geeks_for_Geeks/Practice/Day_6/02_Sum_of_array/01_nearly_sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+	def _sum(self,arr, n):
+            res = 0
+            for i in arr:
+                res += i 
+            return res
+
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        arr=list(map(int , input().strip().split()))
+        ob = Solution()
+        ans = ob._sum(arr, n)
+        print(ans)
+        tc=tc-1
